df8a32d3e98a3e74bc3a97e581c9c129ba681048
mikel.diez <mikeldiezgarcia@gmail.com>
2026-05-21 10:18:20 +0200

    we are on pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ai-somorrostro.github.io",
+  base: "/demostradoriaweb",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
